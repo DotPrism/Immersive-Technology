abe000545824947526dcba6a58875509063a681c
tgstyle <5832709+tgstyle@users.noreply.github.com>
2025-07-20 23:20:13 -0600

    Fix Recipe Serializer for Solar Tower.

--- src/main/java/mctmods/immersivetechnology/core/registration/ITRecipeTypes.java
+++ src/main/java/mctmods/immersivetechnology/core/registration/ITRecipeTypes.java
@@ -13,12 +13,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-@SuppressWarnings("all")
-public class ITRecipeTypes
-{
-    public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(
-            ForgeRegistries.RECIPE_SERIALIZERS, ITLib.MODID
-    );
+public class ITRecipeTypes {
+    public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create( ForgeRegistries.RECIPE_SERIALIZERS, ITLib.MODID );
     private static final DeferredRegister<RecipeType<?>> REGISTER = DeferredRegister.create(Registries.RECIPE_TYPE, ITLib.MODID);
 
     public static final TypeWithClass<AdvancedCokeOvenRecipe> ADVANCED_COKE_OVEN = register("coke_oven_advanced", AdvancedCokeOvenRecipe.class);
@@ -28,25 +24,20 @@ public class ITRecipeTypes
     public static final TypeWithClass<SolarTowerRecipe> SOLAR_TOWER = register("solar_tower", SolarTowerRecipe.class);
     public static final TypeWithClass<? extends Recipe<?>> ADV_COKE_OVEN_FUEL = register("coke_oven_advanced_fuel", AdvancedCokeOvenFuel.class);
 
-    static
-    {
+    static {
         AdvancedCokeOvenRecipe.SERIALIZER = RECIPE_SERIALIZERS.register("coke_oven_advanced", AdvancedCokeOvenRecipeSerializer::new);
         BoilerRecipe.SERIALIZER = RECIPE_SERIALIZERS.register("boiler", BoilerRecipeSerializer::new);
-        SolarTowerRecipe.SERIALIZER = RECIPE_SERIALIZERS.register("boiler", SolarTowerRecipeSerializer::new);
+        SolarTowerRecipe.SERIALIZER = RECIPE_SERIALIZERS.register("solar_tower", SolarTowerRecipeSerializer::new);
         SteamTurbineRecipe.SERIALIZER = RECIPE_SERIALIZERS.register("steam_turbine", SteamTurbineRecipeSerializer::new);
         GasTurbineRecipe.SERIALIZER = RECIPE_SERIALIZERS.register("gas_turbine", GasTurbineRecipeSerializer::new);
     }
 
-    private static <T extends Recipe<?>> TypeWithClass<T> register(String name, Class<T> type)
-    {
-        RegistryObject<RecipeType<T>> regObj = REGISTER.register(name, () -> new RecipeType<>()
-        {
-        });
+    private static <T extends Recipe<?>> TypeWithClass<T> register(String name, Class<T> type) {
+        RegistryObject<RecipeType<T>> regObj = REGISTER.register(name, () -> new RecipeType<>() { });
         return new TypeWithClass<>(regObj, type);
     }
 
-    public static void init()
-    {
+    public static void init() {
         REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
         RECIPE_SERIALIZERS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
